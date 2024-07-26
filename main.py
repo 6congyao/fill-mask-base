a375d6b4b240420ccccddb56e34c64b86dae5da8
Lu Congyao <8966098@qq.com>
2024-07-26 14:44:07 +0800

    release 0.1

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, HTTPException
 from transformers import pipeline
 from pydantic import BaseModel
 from typing import List
-# from paraphraser import paraphrase
 from roberta import unmask
 
 
@@ -16,7 +15,7 @@ app = FastAPI()
 @app.post("/sentences", response_model=SentenceList)
 async def fill_mask_sentences(input: SentenceList):
     try:
-        results = [unmask(s)['sequence'] for s in input.sentences]
+        results = [unmask(s)[1]['sequence'] for s in input.sentences]
         # results = [paraphrase(s)[0] for s in input.sentences]
         # for prediction in results:
         #     print(prediction)
--- roberta.py
+++ roberta.py
@@ -3,17 +3,16 @@ unmasker = pipeline("fill-mask", model="FacebookAI/roberta-base")
 
 
 def unmask(sentence):
-    return unmasker(sentence)[0]
+    return unmasker(sentence)
 
 
 demo_inputs = [
     "Furthermore, <mask> population growth in many developing countries puts a strain on natural resources and infrastructure.",
-    "As populations grow, so does the demand for energy, water, and food, frequently leading to overexploitation and <mask> degradation.",
+    "<mask> Digital devices and online websites are capturing the interests of younger generations as never before and parents are facing more issues when it comes to raising their kids, a lot of which is they're doing through technology.",
     "This <mask> cycle of resource depletion and environmental degradation contributes to rising temperatures and the vulnerability of these regions."
 ]
 
-results = [unmask(s) for s in demo_inputs]
+# results = [unmask(s) for s in demo_inputs]
 
-# print(unmasker(text))
-for prediction in results:
-    print(f"Score: {prediction['score']}, Token: {prediction['token_str']}")
+# for prediction in results:
+#     print(f"Score: {prediction['score']}, Token: {prediction['token_str']}")
